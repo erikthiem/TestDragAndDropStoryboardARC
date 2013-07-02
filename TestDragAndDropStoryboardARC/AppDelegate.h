e966e38d6376f64871893b955b58d639137118ae
Erik Thiem <erik.thiem@dxydoes.com>
2013-07-02 11:56:39 -0400

    Initial Commit

---
+++ TestDragAndDropStoryboardARC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestDragAndDropStoryboardARC
+//
+//  Created by Erik Thiem on 7/2/13.
+//  Copyright (c) 2013 Erik Thiem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestDragAndDropStoryboardARC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestDragAndDropStoryboardARC
+//
+//  Created by Erik Thiem on 7/2/13.
+//  Copyright (c) 2013 Erik Thiem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
